ec5757ba9c9ef84fe0f0b6b8f146d1a33d35d09c
Ironbrain <mfuxic@gmail.com>
2019-03-12 11:37:05 +0900

    管理者チェックを作った

--- JS_Object Tuto/obj2.js
+++ JS_Object Tuto/obj2.js
@@ -3,6 +3,8 @@ let fire = "fire";
 let copy = fire;
 
 //参照渡し
+let admin = {};
+let operator = {};
 let userName = {name:'John'};
 let mamaName = {name:'An'};
 
@@ -15,3 +17,16 @@ alert('numR = ' + numReference.name);
 
 numReference = mamaName;
 alert('numR = ' + numReference.name);
+
+//この参照渡しと厳密等価の仕様を使えば、管理者チェックが作れるのでは？？
+
+operator = userName;
+admin = userName;
+
+operator = mamaName;
+
+if(admin === operator){
+  alert('管理者様、ようこそ！')
+}else{
+  alert('貴様、管理者ではないな！');
+}
