223f38512efff3eacf1a59657100719382e2c921
Ironbrain <mfuxic@gmail.com>
2019-03-17 15:07:18 +0900

    コンストラクタの使い方に一歩踏み込んだ

---
+++ constructor/con3_calc.js
@@ -0,0 +1,41 @@
+function Calc(){
+  let num1 = 0;
+  let num2 = 0;
+
+  this.read = function(){
+    this.num1 = +prompt('num1?',0);
+    this.num2 = +prompt('num2?',0);
+  };
+  this.sum = function(){
+    return this.num1 + this.num2;
+  };
+  this.mul = function(){
+    return this.num1 * this.num2;
+  };
+}
+
+function ProgCalc(defValue) {
+  this.value = defValue;
+
+  this.read = function(){
+    this.value += +prompt('add value?',0);
+  }
+}
+
+const calc = new Calc();
+
+calc.read();
+
+alert(calc.sum() );
+alert(calc.mul() );
+
+//=========================================================================
+
+let prog = new ProgCalc(5);
+
+alert('start prog = ' + prog.value );
+
+prog.read();
+prog.read();
+
+alert('now prog = ' + prog.value );
